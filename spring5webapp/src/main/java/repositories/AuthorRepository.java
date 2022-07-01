ab0703619696d6d2d44c71409abbcb3f1d87ef07
jhatton93 <79428391+jhatton93@users.noreply.github.com>
2022-07-01 11:20:32 -0400

    Add files via upload

---
+++ spring5webapp/src/main/java/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import domain.Author;
+
+public interface AuthorRepository extends CrudRepository<Author, Long>{
+    
+    
+}
